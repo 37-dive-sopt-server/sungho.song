2f98bf8465eccfc7c810fae7b05833890970db5d
송성호 <thdtjdgh1679@naver.com>
2025-10-15 16:53:44 +0900

    feat: 회원가입 시 이메일 중복 검증 추가

--- src/main/java/org/sopt/service/MemberServiceimpl.java
+++ src/main/java/org/sopt/service/MemberServiceimpl.java
@@ -14,10 +14,18 @@ public class MemberServiceimpl implements MemberService {
     private static long sequence = 1L;
 
     public Long join(String name, LocalDate birth, String email, Gender gender) {
+        vaildateDuplication(email);
 
         Member member = new Member(sequence++, name, birth, email, gender);
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void vaildateDuplication(String email) {
+        memberRepository.findByEmail(email)
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 이메일입니다.");
+                });
     }
 
     public Optional<Member> findOne(Long memberId) {
